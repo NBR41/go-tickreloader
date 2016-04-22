08fc4cd952fe6e753acd92647c2881817d14776d
fbernard <fabien.bernard@twenga.com>
2016-04-22 18:27:56 +0200

    first commit

---
+++ example/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/NBR41/tickreloader"
+)
+
+func main() {
+
+	var i = 10
+	var cli = tickreloader.NewClient(
+		3*time.Second,
+		func(v ...interface{}) (interface{}, error) {
+			fmt.Println("!!!!! reload !!!!")
+			var value = v[0].(*int)
+			*value = *value + v[1].(int)
+			return *value, nil
+		},
+		&i, 3,
+	)
+
+	cli.StartTickReload()
+	test(cli)
+	cli.StopTickReload()
+
+	cli = tickreloader.NewClient(
+		1*time.Second,
+		func(v ...interface{}) (interface{}, error) {
+			fmt.Println("!!!!! reload !!!!")
+			return []string{"lala", "lolo"}, nil
+		},
+		&i, 3,
+	)
+
+	cli.StartTickReload()
+	test(cli)
+	cli.StopTickReload()
+}
+
+func test(cli *tickreloader.Client) {
+	var ch = make(chan bool)
+	for j := 0; j < 10; j++ {
+		go func(cli *Client, j int, ch chan bool) {
+			for k := 0; k < 10; k++ {
+				v, err := cli.Get()
+				fmt.Println("j", j, "k", k, "Get", v, err)
+				time.Sleep(1 * time.Second)
+			}
+			if j == 9 {
+				ch <- true
+			}
+		}(cli, j, ch)
+	}
+	<-ch
+}
---
+++ tickreloader.go
@@ -0,0 +1,68 @@
+package tickreloader
+
+import (
+	"sync"
+	"time"
+)
+
+// Client struct for reloader client
+type Client struct {
+	fGetter      getterFunc
+	getterParams []interface{}
+	reloadTick   time.Duration
+	value        interface{}
+	lastError    error
+	loaded       bool
+	exitChan     chan bool
+	sync.Mutex
+}
+
+type getterFunc func(...interface{}) (interface{}, error)
+
+// NewClient return a new Client instance
+func NewClient(reloadTick time.Duration, fGetter getterFunc, getterParams ...interface{}) *Client {
+	return &Client{
+		fGetter:      fGetter,
+		getterParams: getterParams,
+		reloadTick:   reloadTick,
+		exitChan:     make(chan bool),
+	}
+}
+
+// StartTickReload start the reload
+func (c *Client) StartTickReload() {
+	go c.reload()
+}
+
+// StopTickReload stops the reload
+func (c *Client) StopTickReload() {
+	c.exitChan <- true
+}
+
+// Get returns the value
+func (c *Client) Get() (interface{}, error) {
+	c.Lock()
+	defer c.Unlock()
+
+	if !c.loaded {
+		c.value, c.lastError = c.fGetter(c.getterParams...)
+		c.loaded = true
+	}
+
+	return c.value, c.lastError
+}
+
+// reload
+func (c *Client) reload() {
+	tick := time.Tick(c.reloadTick)
+	for {
+		select {
+		case <-c.exitChan:
+			return
+		case <-tick:
+			c.Lock()
+			c.value, c.lastError = c.fGetter(c.getterParams...)
+			c.Unlock()
+		}
+	}
+}
